50c817e8dde5d590bf25ab69d6d8c4813b026000
MarkCarsonDev <markcarson.dev@gmail.com>
2025-04-10 01:54:44 -0700

    Remove all old files

--- sonne/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Sonne - A minimalist static site generator optimized for low footprint sites.
-
-Designed with environmental consciousness in mind, Sonne aims to produce
-efficient websites with minimal resource usage both during generation and hosting.
-"""
-
-__version__ = '0.2.0'
--- sonne/core/__init__.py
+++
@@ -1,1 +0,0 @@
-"""Core functionality for Sonne static site generator."""
